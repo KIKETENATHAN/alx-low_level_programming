7ea1fdde844afb18fdd0b5e751bbbcf950f202b7
KIKETENATHAN <kiketenathan@gmail.com>
2023-07-14 11:20:16 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - main entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
