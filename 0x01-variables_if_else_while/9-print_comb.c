640a01dac81be0fdd372f348339a1e9cf799a339
KIKETENATHAN <kiketenathan@gmail.com>
2023-07-14 12:39:59 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - main entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
