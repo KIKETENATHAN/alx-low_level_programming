7e3245e87e4f51c17ff983943a83f1be5a237754
KIKETENATHAN <kiketenathan@gmail.com>
2023-07-18 09:58:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <main.h>
-#include <stdio.h>
+#include "main.h"
 /**
  * main - main entry point
  *Return: Always 0 for success
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <unistd.h>
 /**
  * _putchar - writes the characters c to stdout 
