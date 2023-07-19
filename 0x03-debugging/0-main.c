88573184353b09bd8bc8ae27a1871760cb8f2538
KIKETENATHAN <kiketenathan@gmail.com>
2023-07-19 01:42:50 -0700

    task 0 debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test function for postive or negative
+ * Return: always  0 success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+debugging directory for c programming
